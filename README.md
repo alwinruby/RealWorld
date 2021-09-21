379301b375d2b690878192751f7c7e4c6bd5fd3e
alwinruby <alwinsolanky@gmail.com>
2021-09-21 13:49:00 +0100

    local and global variables

--- README.md
+++ README.md
@@ -15,3 +15,4 @@ Ingredient is a parameter, or the variable name used to reference the arguments 
 
         Parameter != Argument
 
+Functions search for variable locally first. If the variable is not in the local scope, it expands the search to the global search.
--- part_one/breakfast_function.py
+++ part_one/breakfast_function.py
@@ -1,5 +1,7 @@
 """ A Functional Breakfast """
 
+cheese = 'cheddar'
+
 def mix_and_cook():
     print('Mixing the ingredients')
     print('Greasing the frying pan')
@@ -8,14 +10,16 @@ def mix_and_cook():
     print('Flipping it!')
     print('Cooking the other side')
 
-def make_omelette(ingredient):
+def make_omelette():
+    global cheese
+    cheese = 'swiss'
     mix_and_cook()
-    omelette = 'a {} omelette'.format(ingredient)
+    omelette = 'a {} omelette'.format(cheese)
     return omelette
 
 def make_pancake():
     mix_and_cook()
-    pancake = 'a delicious pancake'
+    pancake = 'a {} pancake'.format(cheese)
     return pancake
 
 def fancy_omelette(*ingredients):
@@ -23,11 +27,11 @@ def fancy_omelette(*ingredients):
     omelette = 'a fancy omelette with {} ingredients'.format(len(ingredients))
     return omelette
 
-# make some regular omelettes
-print(make_omelette('bacon'))
+# make some cheesy foods
+print(cheese)
 print()
-print(make_omelette('spam'))
+print(make_omelette())
 print()
-
-# make Olivia's fancy omelette
-print(fancy_omelette('sausage', 'onion', 'pepper', 'spinach', 'mushroom', 'tomato', 'goat cheese'))
+print(cheese)
+print()
+print(make_pancake())
