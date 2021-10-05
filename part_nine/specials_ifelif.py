d4f590e6d8a4c43a10ac4fc818513b94acfcf7d2
alwinruby <alwinsolanky@gmail.com>
2021-10-05 06:00:12 +0100

    Saturday Pizza

---
+++ part_nine/specials_ifelif.py
@@ -0,0 +1,18 @@
+""" I'll Have the Special! """
+
+today = 'Saturday'
+
+if today == 'Sunday':
+    print('Order the spinach pizza.')
+elif today == 'Monday':
+    print('Order the mushroom pizza.')
+elif today == 'Tuesday':
+    print('Order the pepperoni pizza.')
+elif today == 'Wednesday':
+    print('Order the veggie pizza.')
+elif today == 'Thursday':
+    print('Order the bbq chicken pizza.')
+elif today == 'Friday':
+    print('Order the sausage pizza.')
+elif today == 'Saturday':
+    print('Order the Hawaiian pizza.')
